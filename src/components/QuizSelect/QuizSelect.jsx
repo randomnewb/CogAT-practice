911e78fc0c66c9fbae59851406b09883efc1e62e
randomnewb <nguy1581@gmail.com>
2023-03-12 19:15:49 -0500

    Confirm quiz choice

--- src/components/QuizSelect/QuizSelect.jsx
+++ src/components/QuizSelect/QuizSelect.jsx
@@ -2,9 +2,24 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
+/**
+ * allow the user to confirm their selection before starting the quiz
+ * or cancels the quiz
+ */
+
 function QuizSelect() {
   const history = useHistory();
   const user = useSelector((store) => store.user);
+
+  function confirmQuiz() {
+    let confirm = window.confirm("Are you sure you want to start the quiz?");
+
+    if (confirm) {
+      history.push("/quiz");
+    } else {
+      history.push("/user");
+    }
+  }
 
   return (
     <>
@@ -15,14 +30,23 @@ function QuizSelect() {
         <h3>Select a Quiz</h3>
       </div>
       <div className="container">
-        <button
+        <img
+          src="https://res.cloudinary.com/dartlv0ee/image/upload/v1678664525/images/fast-forward-button_tqs3us.png"
+          alt="Quick Quiz Button"
+          style={{ width: "100px", height: "100px" }}
           onClick={() => {
-            history.push("/quiz");
+            confirmQuiz();
           }}
-        >
-          Quick Quiz
-        </button>
-        <button>Custom Quiz</button>
+        />
+        <br />
+        <img
+          src="https://res.cloudinary.com/dartlv0ee/image/upload/v1678664525/images/gears_1_fgqyqu.png"
+          alt="Custom Quiz Button"
+          style={{ width: "100px", height: "100px" }}
+          // onClick={() => {
+          //   history.push("/quiz");
+          // }}
+        />
       </div>
     </>
   );
